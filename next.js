9a75e8ea4aa4e7a8b7b6f0b5ecd21564fcf43965
oxCucu <0xcucurbitaceae@gmail.com>
2025-04-23 11:27:40 +0200

    adds nextjs base

--- next.js
+++ next.js
@@ -2,9 +2,22 @@ import react from 'eslint-plugin-react'
 import baseRules from './baseRules.js'
 import reactRules from './reactRules.js'
 import globals from 'globals'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
 export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       globals: {
         React: true,
