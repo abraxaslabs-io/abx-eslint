439e13a8073093158985c81cf074beb9c6b474a6
oxCucu <0xcucurbitaceae@gmail.com>
2025-05-03 06:04:01 +0200

    adds tw

--- next.js
+++ next.js
@@ -2,7 +2,7 @@ import react from 'eslint-plugin-react'
 import baseRules from './baseRules.js'
 import reactRules from './reactRules.js'
 import globals from 'globals'
-
+import tailwind from 'eslint-plugin-tailwindcss'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -15,9 +15,21 @@ const compat = new FlatCompat({
 })
 
 export default [
+  ...tailwind.configs['flat/recommended'],
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        React: true,
+        JSX: true,
+        TailwindColor: true,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react,
+    },
     rules: {
       ...baseRules,
       ...reactRules,
